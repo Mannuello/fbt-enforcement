5ab85b169298a36354913f0909e68d7eae012216
Mannuello <128275867+Mannuello@users.noreply.github.com>
2023-10-28 16:09:59 -0700

    Edited file path and altered \n creating logic.
    File path wasn't working on my machine, edited the file path to be more specific within both datatools.py  and test_plate_reader.py
    Added logic to strip newline characters from results.

--- fbt_enforcement/data_tools.py
+++ fbt_enforcement/data_tools.py
@@ -18,7 +18,7 @@ def get_random_plate_path_from_directory() -> str:
     """Returns a random plate image path from a directory of 4 plate images"""
     directory_path = "fbt_enforcement/plates"
     image_files = [
-        file for file in os.listdir(directory_path) if file.endswith((".png"))
+        file for file in os.listdir(directory_path) if file.endswith(".png")
     ]
     plate_path = os.path.normpath(
         os.path.join(directory_path, random.choice(image_files))

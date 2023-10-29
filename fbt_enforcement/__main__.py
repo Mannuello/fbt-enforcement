028b5781a4463fe27521ca726c87104c99fb38f1
Brett <brettllapierre@gmail.com>
2023-10-29 13:03:32 -0700

    Added basic info logging to file
    Added basic info logging to file

--- fbt_enforcement/__main__.py
+++ fbt_enforcement/__main__.py
@@ -1,10 +1,20 @@
+import logging
+from datetime import datetime
 from fbt_enforcement.data_tools import (
     get_all_car_data_records,
     get_random_plate_path_from_directory,
 )
 from fbt_enforcement.plate_reader import get_single_car_data, read_plate_from_image
 
+logging.basicConfig(
+    filename="app.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
+    logger.info("FBT Enforcement app initialized")
     plate_path = get_random_plate_path_from_directory()
     plate_number = read_plate_from_image(plate_path)
     all_car_records = get_all_car_data_records()
--- fbt_enforcement/data_tools.py
+++ fbt_enforcement/data_tools.py
@@ -1,20 +1,27 @@
 import os
 import random
+import logging
 from typing import Any
 
 from .fake_car_data import car_data
 
+logger = logging.getLogger(__name__)
+
 
 def get_all_car_data_records() -> list[dict[str, Any]]:
-    """Brett - Returns all car records"""
-    print("Doing an API call to get all the car data in our system")
+    """Returns all car records"""
+    logger.info("Mock API call retrieved all car data records")
     return car_data
 
 
 def get_random_plate_path_from_directory() -> str:
-    """Brett - Returns a random plate image path from a directory of 4 plate images"""
+    """Returns a random plate image path from a directory of 4 plate images"""
     directory_path = "fbt_enforcement/plates"
     image_files = [
         file for file in os.listdir(directory_path) if file.endswith((".png"))
     ]
-    return os.path.normpath(os.path.join(directory_path, random.choice(image_files)))
+    plate_path = os.path.normpath(
+        os.path.join(directory_path, random.choice(image_files))
+    )
+    logger.info(f"Mock random plate path: {plate_path}")
+    return plate_path

f89401130d16497f55fc58649f3e4e489820febc
Mannuello <128275867+Mannuello@users.noreply.github.com>
2023-10-29 21:59:06 -0700

    Changed directory path handling to be more portable
    Changed directory path handling to be more portable

--- fbt_enforcement/data_tools.py
+++ fbt_enforcement/data_tools.py
@@ -16,12 +16,17 @@ def get_all_car_data_records() -> list[dict[str, Any]]:
 
 def get_random_plate_path_from_directory() -> str:
     """Returns a random plate image path from a directory of 4 plate images"""
-    directory_path = "fbt_enforcement/plates"
-    image_files = [
-        file for file in os.listdir(directory_path) if file.endswith(".png")
-    ]
+
+    # Get the directory where the current script is located
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Define the relative path to the "plates" directory
+    directory_path = os.path.join(script_dir, "plates")
+
+    image_files = [file for file in os.listdir(directory_path) if file.endswith(".png")]
     plate_path = os.path.normpath(
         os.path.join(directory_path, random.choice(image_files))
     )
     logger.info(f"Mock random plate path: {plate_path}")
+
     return plate_path
